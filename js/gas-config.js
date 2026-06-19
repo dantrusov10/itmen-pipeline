19f0507db5e203557873b798b403431f1311c541
dantrusov10 <dantrusov162@gmail.com>
2026-06-19 20:25:51 +0300

    Update GAS config URL to the deployed script URL

--- js/gas-config.js
+++ js/gas-config.js
@@ -3,5 +3,5 @@
  * Скопируйте из: Apps Script → Развернуть → Веб-приложение → URL
  */
 window.ITMEN_GAS_CONFIG = {
-  url: "PASTE_YOUR_APPS_SCRIPT_URL_HERE",
+  url: "https://script.google.com/macros/s/AKfycbyCj4W3I68-oHgYw6zTJ8NYQ5aS1xXUsoFrl0E1zmX0hn8h_fly6U3i5RWOpECfS2yIIg/exec",
 };
